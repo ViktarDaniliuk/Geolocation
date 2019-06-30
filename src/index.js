b9d82726a664e904c915c7de3e7cdd96f4263704
ViktarDaniliuk <daniliukviktar@gmail.com>
2019-06-30 23:45:42 +0200

    после перезагрузки страницы не срабатывает клик по старым меткам

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 ymaps.ready(init);
 
-//var coords;
-
 function init() {
-    var myPlacemark,
-        myMap = new ymaps.Map('map', {
+    let myPlacemark;
+    let coords;
+    let addressText;
+    let popup = false;
+    let myMap = new ymaps.Map('map', {
             center: [52.237750, 21.018374],
             zoom: 17
         });
@@ -13,134 +14,132 @@ function init() {
     myMap.events.add('click', e => {
         // получаем координаты клика
         coords = e.get('coords');
-
-
-//        // на клик по карте показываем popup
-//        document.getElementById('map').addEventListener('click', showPopup);
+        // на клик по карте показываем popup
         showPopup();
+        // полчаем адрес и добавляем его в popup
+        getAddress(coords);
 
-        // создаем placemark на координатах клика (это надо делать после нажатия кнопки "Добавить")
-        myPlacemark = createPlacemark(coords);
-
-
-        // добавляем placemark на карту!!!!!после нажатия на кнопку "Добавить"
-//        myMap.geoObjects
-//            .add(myPlacemark);
-
-//        getAddress(coords);
-
-//        getCoord(coords);
-
-        // появление всплывающего окна
-        // document.querySelector('#popup').classList.toggle('hidden');
-
+        createP();
     })
 
-    // на клик по карте показываем popup
-//    document.getElementById('map').addEventListener('click', showPopup);
+    createAllPlacemark(myMap);
 
-    // на клик по крестике скрываем popup
+    const submitBtn = document.getElementById('submit');
+    const geocode = () => ymaps.geocode(coords).then(function (res) {
+        let comment;
+        let adr;
+        let firstGeoObject = res.geoObjects.get(0);
+
+        adr = firstGeoObject.getAddressLine();
+        comment = localStorage.getItem(adr);
+        createComment(comment);
+        console.log('2');
+    });
+    const handlePlaceMarkClick = e => {
+        coords = e.get('coords');
+        geocode();
+        console.log('1');
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (isFormCompleted()) {
+            addStore(coords);
+            myPlacemark = createPlacemark(coords);
+            myPlacemark.events.add('click', handlePlaceMarkClick);
+
+            myMap.geoObjects.add(myPlacemark);
+            closePopup();
+        }
+    }
+
+    // при нажатии на кнопку "Добавить" проверяем заполнены ли все поля, добавляем placemark на карту, собираем данные с формы,добавляем в localStorage и закрываем окно
+    submitBtn.addEventListener('click', handleSubmit);
+}
+
+// -----------------------------------------------------------------
+// наносим существующие маркеры на карту при первом старте
+function createAllPlacemark(myMap) {
+    let objPlacemarks = localStorage;
+    let coords;
+
+    for (let key in objPlacemarks) {
+        if (JSON.parse(localStorage.getItem(key)) !== null) {
+            coords = JSON.parse(localStorage.getItem(key)).coords;
+            console.log(coords);
+            myPlacemark = createPlacemark(coords);
+            myMap.geoObjects.add(myPlacemark);
+        }
+    }
+}
+// -----------------------------------------------------------------
+
+// на клик по крестику скрываем popup
     document.getElementById('close').addEventListener('click', closePopup);
 
-    // на клик по кнопке "Добавить" сохраняем данные в localStorage
-//    document.getElementById('submit').addEventListener('click', addStore);
+// получаем адрес по координатам и добавляем в форму
+function getAddress(coords) {
+    var adr,
+        myAdr = ymaps.geocode(coords).then(function (res) {
+        var firstGeoObject = res.geoObjects.get(0);
 
-    function getCoord(coords) { // не работает!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        return fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=<dcbcc347-ca94-452b-b918-d0dc3ce72247>&format=json&geocode=${coords}`)
-            .then(response => response.json())
-            .then(res => res)
-        console.log(res);
-    }
-
-
-//    // создаем геообъект (эксперементальный блок)
-//    var myGeoObject = new ymaps.GeoObject({
-//        geometry: {
-//            type: "Point",
-//            coordinates: [52.237750, 21.018374] // координаты точки для которой создали placemark
-//        },
-//        properties: {
-//            iconContent: 'Королевский замок',
-//            hintContent: 'Реально классный замок'
-//        }
-//    }, {
-//            // Иконка метки будет растягиваться под размер ее содержимого.
-//            preset: 'islands#blackStretchyIcon',
-//            draggable: true
-//    });
-//
-//    // добавляем геообъект на карту
-//    myMap.geoObjects
-//        .add(myGeoObject);
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Слушаем клик на карте.
-    myMap.events.add('click', function (e) {
-        var coords = e.get('coords');
-
-
-//        myPlacemark = createPlacemark(coords);
-//        myMap.geoObjects.add(myPlacemark);
-
-        getAddress(coords);
+        // добавляем адресс в форму
+        adr = firstGeoObject.getAddressLine();
+        document.getElementById('address').innerHTML = adr;
     });
+}
 
-    // Создание метки.
-    function createPlacemark(coords) {
-        return new ymaps.Placemark(coords, {}, {
-            preset: 'islands#violetDotIconWithCaption',
-            draggable: false
-        });
-    }
+// создаем параграф с сообщением об отсутствии отзывав
+function createP() {
+    var comments = document.querySelector('.comments');
+    var p = document.createElement('p');
 
-    // Определяем адрес по координатам (обратное геокодирование).
-    function getAddress(coords) {
-        var adr,
-            coordss = coords;
-        myPlacemark.properties.set('iconCaption', 'поиск...');
-        ymaps.geocode(coords).then(function (res) {
-            var firstGeoObject = res.geoObjects.get(0);
-
-            myPlacemark.properties
-                .set({
-                    // Формируем строку с данными об объекте.
-                    iconCaption: [
-                        // Название населенного пункта или вышестоящее административно-территориальное образование.
-                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                    ].filter(Boolean).join(', '),
-                    // В качестве контента балуна задаем строку с адресом объекта.
-                    balloonContent: firstGeoObject.getAddressLine()
-                });
-
-            adr = firstGeoObject.getAddressLine();
-            document.getElementById('adres').innerHTML = adr;
-
-            document.getElementById('submit').addEventListener('click', function(e) {
-                e.preventDefault();
-                addStore(coordss);
-                myPlacemark = createPlacemark(coords);
-                myMap.geoObjects.add(myPlacemark);
-                closePopup.call(this);
-            });
-
-
-        });
+    if (!document.querySelector('.text')) {
+        p.className = 'text';
+        p.innerText = 'Отзывов пока нет...';
+        comments.append(p);
     }
 }
 
+// cоздание метки
+function createPlacemark(coords) {
+    return new ymaps.Placemark(coords, {}, {
+        preset: 'islands#violetDotIconWithCaption',
+        draggable: false
+    });
+}
+
+// создание блока отзыва
+function createComment(commentObj) {
+    let frag = document.createDocumentFragment();
+    let comment = document.createElement('div');
+    let p = document.createElement('p');
+    let name = document.createElement('span');
+    let br = document.createElement('br');
+    let placeName = document.createElement('span');
+    let commentText = document.createElement('p');
+    let comments = document.querySelector('.comments');
+    let pText = document.querySelector('.text');
+
+
+    commentObj = JSON.parse(commentObj);
+    comment.className = 'comment';
+    name.className = 'user-name';
+    name.innerText = commentObj.name;
+    placeName.className = 'place-name';
+    placeName.innerText = commentObj.placeName;
+    commentText.className = 'comment-text';
+    commentText.innerText = commentObj.comment;
+    p.append(name);
+    p.append(br);
+    p.append(placeName);
+    comment.append(p);
+    comment.append(commentText);
+    frag.append(comment);
+    showPopup();
+    comments.append(frag);
+}
 
 // ----------------------------------------------------------------------
 // создать форму и вставить в .container при первой инициализации (т.е. при первом запуске программы)
@@ -160,17 +159,15 @@ function createForm() {
 }
 // -----------------------------------------------------------------------
 
-// -----------------------------------------------------------------------
+// очистка формы после отправки данных в localStorage
 function cleanForm() {
-    // сделать функцию по очистке формы
-
     document.form.reset()
 }
-// -----------------------------------------------------------------------
 
 // -----------------------------------------------------------------------
 // собираем данные со всех полей формы (в т.ч. координаты), создаем объект и добавляем его в localStorage
-function addStore(coordss) {
+function addStore(coords) {
+    let address = document.getElementById('address').textContent;
     let name = document.getElementById('name');
     let placeName = document.getElementById('place-name');
     let comment = document.getElementById('comment');
@@ -179,9 +176,9 @@ function addStore(coordss) {
     commentBlock.name = name.value;
     commentBlock.placeName = placeName.value;
     commentBlock.comment = comment.value;
-    commentBlock.coords = coordss;
-
-    localStorage.setItem('n', JSON.stringify(commentBlock));
+    commentBlock.coords = coords;
+    // если адресс существует, то создавать массив объктов и
+    localStorage.setItem(address, JSON.stringify(commentBlock));
     cleanForm();
 }
 // -----------------------------------------------------------------------
@@ -193,59 +190,57 @@ function getStore() {
 }
 // -----------------------------------------------------------------------
 
-//// создаем Placemark
-//function createPlacemark(coords) {
-//    return new ymaps.Placemark(coords, {
-//        iconCaption: 'поиск...'
-//    }, {
-//        preset: 'islands#violetDotIconWithCaption',
-//        draggable: false
-//    });
-//}
+// проверяем заполнены ли все поля формы
+function isFormCompleted() {
+    let name = document.getElementById('name').value;
+    let placeName = document.getElementById('place-name').value;
+    let comment = document.getElementById('comment').value;
+
+    if (Boolean(name) && Boolean(placeName) && Boolean(comment)) {
+        return true;
+    }
+    alert('Заполните все поля формы');
+
+    return false;
+}
 
 // показываем popup
 function showPopup() {
     var form = document.querySelector('.form-wrapper');
 
     form.classList.remove('hidden');
+    document.getElementById('map').onclick = function(e) {
+            var top = e.clientY;
+            var left = e.clientX;
+            var screenHeight = window.innerHeight;
+            var screenWidth = window.innerWidth;
 
-    document.getElementById('map').addEventListener('click', function(e) {
-        var top = e.clientY;
-        var left = e.clientX;
-        var screenHeight = window.innerHeight;
-        var screenWidth = window.innerWidth;
+            // контролируем, что бы popup не выходило за пределы карты по высоте
+            if (top < 205) {
+                form.style.top = 5 + 'px';
+            } else
+            if (screenHeight - top < 325) {
+                form.style.top = screenHeight - 535 + 'px';
+            } else {
+                form.style.top = top - 200 + 'px';
+            }
 
-        // контролируем, что бы popup не выходило за пределы карты по высоте
-        if (top < 205) {
-            form.style.top = 5 + 'px';
-        } else
-        if (screenHeight - top < 325) {
-            form.style.top = screenHeight - 535 + 'px';
-        } else {
-            form.style.top = top - 200 + 'px';
-        }
-
-        // контролируем, что бы popup не выходило за пределы карты по ширине
-        if (left < 455) {
-            form.style.left = left + 80 + 'px';
-        } else {
-            form.style.left = left - 450 + 'px';
-        }
-    })
+            // контролируем, что бы popup не выходило за пределы карты по ширине
+            if (left < 455) {
+                form.style.left = left + 80 + 'px';
+            } else {
+                form.style.left = left - 450 + 'px';
+            }
+    }
 }
 
 // закрываем popup
 function closePopup() {
     var comments = document.querySelector('.comments');
 
-    if (this.id === 'close') {
-        this.parentElement.parentElement.classList.add('hidden');
-    } else
-    if (this.id === 'submit') {
-        this.parentElement.parentElement.parentElement.classList.add('hidden');
-    }
+    comments.parentElement.parentElement.classList.add('hidden');
 
-    // чистим div с комментариями
+    // чистим div с комментариями (лучше при помощи функции очистить и комментарии и форму)
     comments.innerHTML = '';
 }
 
