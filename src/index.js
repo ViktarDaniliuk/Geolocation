b842c5f1c54d7321b9ca0ce75637a67babd9fe1b
ViktarDaniliuk <daniliukviktar@gmail.com>
2019-07-13 19:46:18 +0200

    6

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-function init() {
+const init = () => {
     let myPlacemark;
     let coords;
-    let myMap = new ymaps.Map('map', {
-            center: [52.237750, 21.018374],
-            zoom: 17
-        });
-    let customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+    const myMap = new ymaps.Map('map', {
+        center: [52.237750, 21.018374],
+        zoom: 17
+    });
+    const customItemContentLayout = ymaps.templateLayoutFactory.createClass(
         '<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
             '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
             '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
     );
-    let clusterer = new ymaps.Clusterer({
+    const clusterer = new ymaps.Clusterer({
         preset: 'islands#invertedVioletClusterIcons',
         clusterDisableClickZoom: true,
         clusterOpenBalloonOnClick: true,
@@ -24,22 +24,22 @@ function init() {
         geoObjectHideIconOnBalloonOpen: false,
         placemarkOpenBalloonOnClick: false
     });
-    let getPointData = function (key, placeName, comment, date) {
-            return {
-                balloonContentBody: '<div style="height: 130px"><p style="line-height: 28px"><strong>' + placeName + '</strong></p><p><a href="#">' + key + '</a></p><p style="line-height: 28px">' + comment + '</p></div>',
-                balloonContentFooter: '<p style="text-align: right; font-size: 12px">' + date + '</p>'
-            };
+    const getPointData = (key, placeName, comment, date) => {
+        return {
+            balloonContentBody: '<div style="height: 130px"><p style="line-height: 28px"><strong>' + placeName + '</strong></p><p><a href="#">' + key + '</a></p><p style="line-height: 28px">' + comment + '</p></div>',
+            balloonContentFooter: '<p style="text-align: right; font-size: 12px">' + date + '</p>'
         };
-    let getPointOptions = function () {
-            return {
-                preset: 'islands#violetDotIconWithCaption',
-                draggable: false,
-                hasBalloon: false
-            };
+    };
+    const getPointOptions = () => {
+        return {
+            preset: 'islands#violetDotIconWithCaption',
+            draggable: false,
+            hasBalloon: false
         };
-    let objPlacemarks = localStorage;
+    };
+    const objPlacemarks = localStorage;
     const submitBtn = document.getElementById('submit');
-    const geocode = (address) => ymaps.geocode(coords).then(function (res) {
+    const geocode = (address) => ymaps.geocode(coords).then((res) => {
         let commentsBlock;
         let adr;
         let firstGeoObject = res.geoObjects.get(0);
@@ -76,20 +76,11 @@ function init() {
             closePopup();
         }
     };
-    function addAllPlacemark() {
+    const addAllPlacemark = () => {
         for (let key in objPlacemarks) {
-            let coords;
-            let placeName;
-            let comment;
-
             if (localStorage.length === 0) return;
             if (JSON.parse(localStorage.getItem(key)) !== null) {
-
-                JSON.parse(localStorage.getItem(key)).forEach(function(item, i, arr) {
-                    coords = item.coords;
-                    placeName = item.placeName;
-                    comment = item.comment;
-                    date = item.date;
+                JSON.parse(localStorage.getItem(key)).forEach(({coords, placeName, comment, date}) => {
                     myPlacemark = createPlacemark(coords, getPointData(key, placeName, comment, date), getPointOptions());
                     myPlacemark.events.add('click', handlePlaceMarkClick);
                     myMap.geoObjects.add(myPlacemark);
@@ -101,7 +92,7 @@ function init() {
         myMap.geoObjects.add(clusterer);
     }
 
-    function openPopupFromBalloon(e) {
+    const openPopupFromBalloon = e => {
         if (e.target.hasAttribute('href')) {
             let key = e.target.textContent;
             let comment;
@@ -114,14 +105,8 @@ function init() {
         }
     }
 
-    clusterer.options.set({
-        gridSize: 80,
-        clusterDisableClickZoom: true
-    });
-
     // вешаем событие на клик по карте
     myMap.events.add('click', e => {
-
         // получаем координаты клика
         coords = e.get('coords');
         // на клик по карте показываем popup
@@ -133,11 +118,11 @@ function init() {
     });
 
     // скрываем popup при клике по кластеру
-    myMap.events.add('balloonopen', function (e) {
+    myMap.events.add('balloonopen', () => {
         closePopup();
     });
 
-    myMap.events.add('click', function (e) {
+    myMap.events.add('click', () => {
         if (!!myMap.balloon.isOpen()) {
             myMap.balloon.close();
         }
@@ -145,7 +130,6 @@ function init() {
 
     addAllPlacemark();
 
-    // при нажатии на кнопку "Добавить" проверяем заполнены ли все поля, добавляем placemark на карту, собираем данные с формы,добавляем в localStorage и закрываем окно
     submitBtn.addEventListener('click', handleSubmit);
 
     document.getElementById('map').addEventListener('click', positioningPopup);
@@ -155,14 +139,12 @@ function init() {
 
 ymaps.ready(init);
 
-// на клик по крестику скрываем popup
-    document.getElementById('close').addEventListener('click', closePopup);
-
 // получаем адрес по координатам и добавляем в форму
-function getAddress(coords) {
-    var adr;
-    var myAdr = ymaps.geocode(coords).then(function (res) {
-        var firstGeoObject = res.geoObjects.get(0);
+const getAddress = coords => {
+    let adr;
+
+    myAdr = ymaps.geocode(coords).then((res) => {
+        let firstGeoObject = res.geoObjects.get(0);
 
         // добавляем адресс в форму
         adr = firstGeoObject.getAddressLine();
@@ -171,7 +153,8 @@ function getAddress(coords) {
 }
 
 // получаем текущую дату и время
-function getDate() {
+// !!!!!!!!!!!!!!!!Переделать эту функцию при помощи фреймворка (по созвону)
+const getDate = () => {
     let date = new Date();
     let currentYear = date.getFullYear();
     let currentMonth = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
@@ -185,9 +168,9 @@ function getDate() {
 }
 
 // создаем параграф с сообщением об отсутствии отзывав
-function createP() {
-    var comments = document.querySelector('.comments');
-    var p = document.createElement('p');
+const createP = () => {
+    let comments = document.querySelector('.comments');
+    let p = document.createElement('p');
 
     if (!document.querySelector('.text')) {
         comments.innerHTML = '';
@@ -198,19 +181,18 @@ function createP() {
 }
 
 // cоздание метки
-function createPlacemark(coords, fn, fnc) {
-//    console.log('createPlacemark');
+const createPlacemark = (coords, fn, fnc) => {
     return new ymaps.Placemark(coords, fn, fnc);
 }
 
 // создание блока сожержащего блоки отдельных отзывов
-function createComments(commentsArray) {
+const createComments = commentsArray => {
     let frag = document.createDocumentFragment();
     let comments = document.querySelector('.comments');
     let comment;
 
     commentsArray = JSON.parse(commentsArray);
-    commentsArray.forEach(function(item, i, arr){
+    commentsArray.forEach((item) => {
         comment = createComment(item);
         frag.append(comment);
     })
@@ -220,16 +202,17 @@ function createComments(commentsArray) {
 }
 
 // создание блока отзыва
-function createComment(commentObj) {
-    let comment = document.createElement('div');
-    let p = document.createElement('p');
-    let name = document.createElement('span');
-    let placeName = document.createElement('span');
-    let commentText = document.createElement('p');
-    let pText = document.querySelector('.text');
-    let date = document.createElement('span');
-    let firstSpace = document.createElement('span');
-    let secondSpace = document.createElement('span');
+// !!!!!!!!!!!!!!!!!!!! Вместо создания кождого элемента по отдельности вставить кусок HTML кода
+const createComment = commentObj => {
+    let doc = document;
+    let comment = doc.createElement('div');
+    let p = doc.createElement('p');
+    let name = doc.createElement('span');
+    let placeName = doc.createElement('span');
+    let commentText = doc.createElement('p');
+    let date = doc.createElement('span');
+    let firstSpace = doc.createElement('span');
+    let secondSpace = doc.createElement('span');
 
     comment.className = 'comment';
     name.className = 'user-name';
@@ -255,8 +238,7 @@ function createComment(commentObj) {
 
 // ----------------------------------------------------------------------
 // создать форму и вставить в .container при первой инициализации (т.е. при первом запуске программы)
-function createForm() {
-    console.log('createForm');
+const createForm = () => {
     let form = document.createElement('form');
     let inputName = document.createElement('input');
     let inputPlace = document.createElement('input');
@@ -273,12 +255,12 @@ function createForm() {
 // -----------------------------------------------------------------------
 
 // очистка формы после отправки данных в localStorage
-function cleanForm() {
+const cleanForm = () => {
     document.form.reset();
 }
 
 // собираем данные со всех полей формы (в т.ч. координаты), создаем объект и добавляем его в localStorage
-function addStore(coords) {
+const addStore = coords => {
     let address = document.getElementById('address').textContent;
     let name = document.getElementById('name');
     let placeName = document.getElementById('place-name');
@@ -312,7 +294,7 @@ function addStore(coords) {
 }
 
 // проверяем заполнены ли все поля формы
-function isFormCompleted() {
+const isFormCompleted = () => {
     let name = document.getElementById('name').value;
     let placeName = document.getElementById('place-name').value;
     let comment = document.getElementById('comment').value;
@@ -326,23 +308,22 @@ function isFormCompleted() {
 }
 
 // показываем popup
-function showPopup() {
-    var form = document.querySelector('.form-wrapper');
+const showPopup = () => {
+    let form = document.querySelector('.form-wrapper');
 
     form.classList.remove('hidden');
 }
 
 // позиционирование popup
-function positioningPopup(e) {
-    var top = e.clientY;
-    var left = e.clientX;
-    var screenHeight = window.innerHeight;
-    var screenWidth = window.innerWidth;
-    var form = document.querySelector('.form-wrapper');
+const positioningPopup = e => {
+    let top = e.clientY;
+    let left = e.clientX;
+    let screenHeight = window.innerHeight;
+    let form = document.querySelector('.form-wrapper');
 
     // контролируем, что бы popup не выходило за пределы карты по высоте
     if (top < 205) {
-                form.style.top = 5 + 'px';
+        form.style.top = 5 + 'px';
     } else
     if (screenHeight - top < 325) {
         form.style.top = screenHeight - 535 + 'px';
@@ -359,8 +340,8 @@ function positioningPopup(e) {
 }
 
 // закрываем popup
-function closePopup() {
-    var comments = document.querySelector('.comments');
+const closePopup = () => {
+    let comments = document.querySelector('.comments');
 
     comments.parentElement.parentElement.classList.add('hidden');
     // чистим div с комментариями (лучше при помощи функции очистить и комментарии и форму)
@@ -368,11 +349,23 @@ function closePopup() {
 }
 
 // получаем размер экрана
-function setSizeMap() {
-    var screenHeight = window.innerHeight;
-    var container = document.querySelector('.container');
+const setSizeMap = () => {
+    let screenHeight = window.innerHeight;
+    let container = document.querySelector('.container');
 
     container.style.height = screenHeight + 'px';
 }
 
+// на клик по крестику скрываем popup
+document.getElementById('close').addEventListener('click', closePopup);
+
 setSizeMap();
+
+// разбить на блоки и сделать импорт
+// попробовать вынести document в переменную в блок init и убрать document изо всех функций.
+// обязатьельно сделать сборку webpack
+// обязательно использовать hbs для создания html документа и html блоков
+
+// по реакту изучить документация getstsrted
+// webpak попробовать по документации getstarted
+// по второму проекту развернуть експрес, сокет ай о и на хабре статья есть, подключить базу данных 'монго' и фреймворк 'мангус'. На эту тему есть созвон с Антоном.
